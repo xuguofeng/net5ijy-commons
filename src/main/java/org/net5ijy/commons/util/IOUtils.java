1c7b975d87131609027b72cfb68973941079c3c7
xuguofeng <xuguofeng1990@126.com>
2019-05-20 08:56:10 +0800

    添加配置文件输入流加载工具方法

--- src/main/java/org/net5ijy/commons/util/IOUtils.java
+++ src/main/java/org/net5ijy/commons/util/IOUtils.java
@@ -1,7 +1,10 @@
 package org.net5ijy.commons.util;
 
 import java.io.Closeable;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class IOUtils {
 
@@ -12,5 +15,30 @@ public class IOUtils {
 			} catch (IOException e) {
 			}
 		}
+	}
+
+	/**
+	 * 从classpath或${user.dir}/config目录下加载配置文件输入流
+	 * 
+	 * @return
+	 */
+	public static InputStream resourceInputStream(String filename) {
+
+		InputStream in = null;
+
+		try {
+
+			in = IOUtils.class.getClassLoader().getResourceAsStream(filename);
+
+			if (in == null) {
+				in = new FileInputStream(System.getProperty("user.dir") + File.separator + "config"
+						+ File.separator + filename);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return in;
 	}
 }
